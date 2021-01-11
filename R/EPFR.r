abf9472130eeb707d3ac46643eac3cb6c80b4543
Vikram K. Srimurthy <Vikram.C.Srimurthy@epfrglobal.com>
2021-01-11 15:40:16 -0500

    Version 1.0.0

--- R/EPFR.r
+++ R/EPFR.r
@@ -4320,9 +4320,14 @@ ftp.dir <- function (x, y, n, w, h = F, u = 60)
     if (is.null(y)) 
         stop("Error in ftp.dir. Remote folder ", x, " does not exist ...")
     y <- ftp.dir.excise.crap(y, "150 Opening data channel for directory listing")
-    if (length(y) > 0) 
+    if (length(y) > 1) {
         y <- y[txt.left(y, 1) != "-" | txt.parse(txt.itrim(y), 
             txt.space(1))[, 5] != 0]
+    }
+    else if (length(y) == 1) {
+        y <- y[txt.left(y, 1) != "-" | txt.parse(txt.itrim(y), 
+            txt.space(1))[5] != 0]
+    }
     if (length(y) > 0) {
         n <- min(nchar(y)) - 4
         while (any(!is.element(substring(y, n, n + 4), paste0(" ", 
